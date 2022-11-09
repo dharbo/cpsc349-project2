48ea70af03d85eee599356d6523fced36bbd2b48
KevinDelgad <kevindel891@gmail.com>
2022-11-08 17:30:26 -0800

    add hv measurements

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         '128': '32rem',
         '4800': "75rem"
       },
+      height: {
+				"10v": "10vh",
+				"20v": "20vh",
+				"30v": "30vh",
+				"40v": "40vh",
+				"50v": "50vh",
+				"60v": "60vh",
+				"70v": "70vh",
+				"80v": "80vh",
+				"90v": "90vh",
+			},
     },
   },
   plugins: [require("daisyui")],
